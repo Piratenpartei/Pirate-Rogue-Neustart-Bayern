608191b99677926aabd82ee8e2a4ae955585c5d1
Josef Reichardt <reichardt.josef+git@gmail.com>
2018-06-18 16:59:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pirate-Rogue - LTW18
+
+WordPress Theme für die Landtagswahl 2018 in Bayern basierend auf
+[Pirate-Rogue](https://github.com/Piratenpartei/Pirate-Rogue) (xwolf).
+
+## Lokale Entwicklung
+
+1. ZIP herunterladen: https://github.com/Piratenpartei/Pirate-Rogue/archive/master.zip
+2. In Verzeichnis `pirate-rogue` entpacken
+3. `docker-compose up -d`
---
+++ pirate-rogue-ltw18-child/functions.php
@@ -0,0 +1,24 @@
+<?php
+function pirate_rogue_ltw18_enqueue_styles() {
+  $parent_style = 'pirate-rogue-style';
+
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'child-style',
+      get_stylesheet_directory_uri() . '/style.css',
+      array( $parent_style ),
+      wp_get_theme()->get('Version')
+  );
+}
+add_action( 'wp_enqueue_scripts', 'pirate_rogue_ltw18_enqueue_styles' );
+
+$blogname = get_bloginfo('name');
+set_theme_mod( 'pirate_rogue_credit',
+  sprintf(
+    'Copyright &copy; %1$s %2$s<br /> '
+    . 'Made with &#x2665; and <a href="https://wordpress.org/" rel="nofollow">WordPress</a>. '
+    . 'Theme: <a href="https://gitlab.com/CptS/ltw2018-wordpress-theme" rel="nofollow">Pirate Rogue LTW18</a> by CptS based on <a href="https://github.com/Piratenpartei/Pirate-Rogue/" rel="nofollow">Pirate Rogue</a> by xwolf.',
+    date("Y"),
+    $blogname
+  )
+);
+?>
