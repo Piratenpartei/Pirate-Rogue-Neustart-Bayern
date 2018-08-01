13ec493b9d58524d1c71f4c690a810cd3df7d91b
Josef Reichardt <reichardt.josef+gitlab@gmail.com>
2018-08-01 17:28:55 +0200

    Topic_Widget optimized

--- pirate-rogue-ltw18-child/Topic_Widget.php
+++ pirate-rogue-ltw18-child/Topic_Widget.php
@@ -41,52 +41,52 @@ class Topic_Widget extends WP_Widget
         echo $args['after_widget'];
         ?>
         <script>
-            (function (id) {
+            (function (id, links) {
                 var imgWrapper = document.getElementById(id + '-wrapper');
                 var linksWrapper = document.getElementById(id + '-links');
                 var img = imgWrapper.querySelector('img');
-                var links = {
-                    '/wahlprogramm-2018/familienpolitik': [
+                var topicCoords = {
+                    'familienpolitik': [
                         [2, 6, 36, 11],
                         [5, 11, 15, 16]
                     ],
-                    '/wahlprogramm-2018/freiheit-sicherheit-rechtsstaat': [
+                    'freiheitsicherheitrechtsstaat': [
                         [42, 1, 100, 11]
                     ],
-                    '/wahlprogramm-2018/heimat': [
+                    'heimat': [
                         [28, 16, 43, 26],
                         [43, 19, 57, 24]
                     ],
-                    '/wahlprogramm-2018/digitale-wirtschaft': [
+                    'digitalewirtschaft': [
                         [59, 25, 66, 30],
                         [66, 26, 92, 33]
                     ],
-                    '/wahlprogramm-2018/bezahlbarer-wohnraum': [
+                    'bezahlbarerwohnraum': [
                         [5, 33, 33, 40],
                         [6, 40, 16, 46]
                     ],
-                    '/wahlprogramm-2018/digitales-bayern': [
+                    'digitalesbayern': [
                         [76, 39, 97, 46],
                         [79, 46, 94, 55]
                     ],
-                    '/wahlprogramm-2018/fluch': [
+                    'flucht': [
                         [10, 55, 20, 63],
                         [20, 58, 37, 62]
                     ],
-                    '/wahlprogramm-2018/verkehrspolitik': [
+                    'verkehrspolitik': [
                         [71, 62, 80, 68],
                         [62, 68, 98, 72]
                     ],
-                    '/wahlprogramm-2018/zukunftsgerechte-bildung': [
+                    'zukunftsgerechtebildung': [
                         [25, 75, 39, 84],
                         [39, 77, 79, 83]
                     ],
                     // TODO: "Energiewirtschaft" --> "Energie"????
-                    '/wahlprogramm-2018/energie': [
+                    'energie': [
                         [2, 83, 11, 88],
                         [2, 88, 43, 92]
                     ],
-                    '/wahlprogramm-2018/verbraucher-und-datenschutz': [
+                    'verbraucherunddatenschutz': [
                         [51, 90, 98, 97]
                     ]
                 };
@@ -96,11 +96,12 @@ class Topic_Widget extends WP_Widget
                     linksWrapper.style.left = ((imgWrapper.offsetWidth - img.width) / 2) + 'px';
 
                     linksWrapper.innerHTML = '';
-                    for (var link in links) {
-                        if (links.hasOwnProperty(link)) {
-                            for (var i = 0; i < links[link].length; i++) {
-                                var coords = links[link][i];
-                                linksWrapper.innerHTML += '<a href="' + link + '" ' +
+                    for (var topic in topicCoords) {
+                        if (topicCoords.hasOwnProperty(topic)) {
+                            for (var i = 0; i < topicCoords[topic].length; i++) {
+                                var coords = topicCoords[topic][i];
+                                console.log(topic,links[topic]);
+                                linksWrapper.innerHTML += '<a href="' + links[topic] + '" ' +
                                     'style="display: block; position: absolute; ' +
                                     'left: ' + coords[0] + '%; top: ' + coords[1] + '%;' +
                                     'width: ' + (coords[2] - coords[0]) + '%; height: ' + (coords[3] - coords[1]) + '%;">&nbsp;</a>';
@@ -112,7 +113,19 @@ class Topic_Widget extends WP_Widget
                 applyImageSize();
                 img.onload = applyImageSize;
                 window.addEventListener('resize', applyImageSize, true);
-            }('<?php echo $id; ?>'))
+            }('<?php echo $id; ?>', {
+                familienpolitik: '<?php echo esc_attr($instance['link_familienpolitik']); ?>',
+                freiheitsicherheitrechtsstaat: '<?php echo esc_attr($instance['link_freiheitsicherheitrechtsstaat']); ?>',
+                heimat: '<?php echo esc_attr($instance['link_heimat']); ?>',
+                digitalewirtschaft: '<?php echo esc_attr($instance['link_digitalewirtschaft']); ?>',
+                bezahlbarerwohnraum: '<?php echo esc_attr($instance['link_bezahlbarerwohnraum']); ?>',
+                digitalesbayern: '<?php echo esc_attr($instance['link_digitalesbayern']); ?>',
+                flucht: '<?php echo esc_attr($instance['link_flucht']); ?>',
+                verkehrspolitik: '<?php echo esc_attr($instance['link_verkehrspolitik']); ?>',
+                zukunftsgerechtebildung: '<?php echo esc_attr($instance['link_zukunftsgerechtebildung']); ?>',
+                energie: '<?php echo esc_attr($instance['link_energie']); ?>',
+                verbraucherunddatenschutz: '<?php echo esc_attr($instance['link_verbraucherunddatenschutz']); ?>'
+            }))
         </script>
         <?php
     }
@@ -124,12 +137,113 @@ class Topic_Widget extends WP_Widget
         } else {
             $title = __('Themen', 'pirate_rouge_ltw18_topic_widget');
         }
+        if (isset($instance['link_familienpolitik'])) {
+            $link_familienpolitik = $instance['link_familienpolitik'];
+        } else {
+            $link_familienpolitik = __('/wahlprogramm-2018/familienpolitik', 'pirate_rouge_ltw18_topic_widget');
+        }
+        if (isset($instance['link_freiheitsicherheitrechtsstaat'])) {
+            $link_freiheitsicherheitrechtsstaat = $instance['link_freiheitsicherheitrechtsstaat'];
+        } else {
+            $link_freiheitsicherheitrechtsstaat = __('/wahlprogramm-2018/freiheit-sicherheit-rechtsstaat', 'pirate_rouge_ltw18_topic_widget');
+        }
+        if (isset($instance['link_heimat'])) {
+            $link_heimat = $instance['link_heimat'];
+        } else {
+            $link_heimat = __('/wahlprogramm-2018/heimat', 'pirate_rouge_ltw18_topic_widget');
+        }
+        if (isset($instance['link_digitalewirtschaft'])) {
+            $link_digitalewirtschaft = $instance['link_digitalewirtschaft'];
+        } else {
+            $link_digitalewirtschaft = __('/wahlprogramm-2018/digitale-wirtschaft', 'pirate_rouge_ltw18_topic_widget');
+        }
+        if (isset($instance['link_bezahlbarerwohnraum'])) {
+            $link_bezahlbarerwohnraum = $instance['link_bezahlbarerwohnraum'];
+        } else {
+            $link_bezahlbarerwohnraum = __('/wahlprogramm-2018/bezahlbarer-wohnraum', 'pirate_rouge_ltw18_topic_widget');
+        }
+        if (isset($instance['link_digitalesbayern'])) {
+            $link_digitalesbayern = $instance['link_digitalesbayern'];
+        } else {
+            $link_digitalesbayern = __('/wahlprogramm-2018/digitales-bayern', 'pirate_rouge_ltw18_topic_widget');
+        }
+        if (isset($instance['link_flucht'])) {
+            $link_flucht = $instance['link_flucht'];
+        } else {
+            $link_flucht = __('/wahlprogramm-2018/flucht', 'pirate_rouge_ltw18_topic_widget');
+        }
+        if (isset($instance['link_verkehrspolitik'])) {
+            $link_verkehrspolitik = $instance['link_verkehrspolitik'];
+        } else {
+            $link_verkehrspolitik = __('/wahlprogramm-2018/verkehrspolitik', 'pirate_rouge_ltw18_topic_widget');
+        }
+        if (isset($instance['link_zukunftsgerechtebildung'])) {
+            $link_zukunftsgerechtebildung = $instance['link_zukunftsgerechtebildung'];
+        } else {
+            $link_zukunftsgerechtebildung = __('/wahlprogramm-2018/zukunftsgerechtebildung', 'pirate_rouge_ltw18_topic_widget');
+        }
+        if (isset($instance['link_energie'])) {
+            $link_energie = $instance['link_energie'];
+        } else {
+            $link_energie = __('/wahlprogramm-2018/energie', 'pirate_rouge_ltw18_topic_widget');
+        }
+        if (isset($instance['link_verbraucherunddatenschutz'])) {
+            $link_verbraucherunddatenschutz = $instance['link_verbraucherunddatenschutz'];
+        } else {
+            $link_verbraucherunddatenschutz = __('/wahlprogramm-2018/verbraucherunddatenschutz', 'pirate_rouge_ltw18_topic_widget');
+        }
         ?>
         <p>
             <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
             <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
                    name="<?php echo $this->get_field_name('title'); ?>" type="text"
                    value="<?php echo esc_attr($title); ?>"/>
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('link_familienpolitik'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('link_familienpolitik'); ?>"
+                   name="<?php echo $this->get_field_name('link_familienpolitik'); ?>" type="text"
+                   value="<?php echo esc_attr($link_familienpolitik); ?>"/>
+            <label for="<?php echo $this->get_field_id('link_freiheitsicherheitrechtsstaat'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('link_freiheitsicherheitrechtsstaat'); ?>"
+                   name="<?php echo $this->get_field_name('link_freiheitsicherheitrechtsstaat'); ?>" type="text"
+                   value="<?php echo esc_attr($link_freiheitsicherheitrechtsstaat); ?>"/>
+            <label for="<?php echo $this->get_field_id('link_heimat'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('link_heimat'); ?>"
+                   name="<?php echo $this->get_field_name('link_heimat'); ?>" type="text"
+                   value="<?php echo esc_attr($link_heimat); ?>"/>
+            <label for="<?php echo $this->get_field_id('link_digitalewirtschaft'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('link_digitalewirtschaft'); ?>"
+                   name="<?php echo $this->get_field_name('link_digitalewirtschaft'); ?>" type="text"
+                   value="<?php echo esc_attr($link_digitalewirtschaft); ?>"/>
+            <label for="<?php echo $this->get_field_id('link_bezahlbarerwohnraum'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('link_bezahlbarerwohnraum'); ?>"
+                   name="<?php echo $this->get_field_name('link_bezahlbarerwohnraum'); ?>" type="text"
+                   value="<?php echo esc_attr($link_bezahlbarerwohnraum); ?>"/>
+            <label for="<?php echo $this->get_field_id('link_digitalesbayern'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('link_digitalesbayern'); ?>"
+                   name="<?php echo $this->get_field_name('link_digitalesbayern'); ?>" type="text"
+                   value="<?php echo esc_attr($link_digitalesbayern); ?>"/>
+            <label for="<?php echo $this->get_field_id('link_flucht'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('link_flucht'); ?>"
+                   name="<?php echo $this->get_field_name('link_flucht'); ?>" type="text"
+                   value="<?php echo esc_attr($link_flucht); ?>"/>
+            <label for="<?php echo $this->get_field_id('link_verkehrspolitik'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('link_verkehrspolitik'); ?>"
+                   name="<?php echo $this->get_field_name('link_verkehrspolitik'); ?>" type="text"
+                   value="<?php echo esc_attr($link_verkehrspolitik); ?>"/>
+            <label for="<?php echo $this->get_field_id('link_zukunftsgerechtebildung'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('link_zukunftsgerechtebildung'); ?>"
+                   name="<?php echo $this->get_field_name('link_zukunftsgerechtebildung'); ?>" type="text"
+                   value="<?php echo esc_attr($link_zukunftsgerechtebildung); ?>"/>
+            <label for="<?php echo $this->get_field_id('link_energie'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('link_energie'); ?>"
+                   name="<?php echo $this->get_field_name('link_energie'); ?>" type="text"
+                   value="<?php echo esc_attr($link_energie); ?>"/>
+            <label for="<?php echo $this->get_field_id('link_verbraucherunddatenschutz'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('link_verbraucherunddatenschutz'); ?>"
+                   name="<?php echo $this->get_field_name('link_verbraucherunddatenschutz'); ?>" type="text"
+                   value="<?php echo esc_attr($link_verbraucherunddatenschutz); ?>"/>
         </p>
         <?php
     }
@@ -138,6 +252,19 @@ class Topic_Widget extends WP_Widget
     {
         $instance = array();
         $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+
+        $instance['link_familienpolitik'] = (!empty($new_instance['link_familienpolitik'])) ? strip_tags($new_instance['link_familienpolitik']) : '';
+        $instance['link_freiheitsicherheitrechtsstaat'] = (!empty($new_instance['link_freiheitsicherheitrechtsstaat'])) ? strip_tags($new_instance['link_freiheitsicherheitrechtsstaat']) : '';
+        $instance['link_heimat'] = (!empty($new_instance['link_heimat'])) ? strip_tags($new_instance['link_heimat']) : '';
+        $instance['link_digitalewirtschaft'] = (!empty($new_instance['link_digitalewirtschaft'])) ? strip_tags($new_instance['link_digitalewirtschaft']) : '';
+        $instance['link_bezahlbarerwohnraum'] = (!empty($new_instance['link_bezahlbarerwohnraum'])) ? strip_tags($new_instance['link_bezahlbarerwohnraum']) : '';
+        $instance['link_digitalesbayern'] = (!empty($new_instance['link_digitalesbayern'])) ? strip_tags($new_instance['link_digitalesbayern']) : '';
+        $instance['link_flucht'] = (!empty($new_instance['link_flucht'])) ? strip_tags($new_instance['link_flucht']) : '';
+        $instance['link_verkehrspolitik'] = (!empty($new_instance['link_verkehrspolitik'])) ? strip_tags($new_instance['link_verkehrspolitik']) : '';
+        $instance['link_zukunftsgerechtebildung'] = (!empty($new_instance['link_zukunftsgerechtebildung'])) ? strip_tags($new_instance['link_zukunftsgerechtebildung']) : '';
+        $instance['link_energie'] = (!empty($new_instance['link_energie'])) ? strip_tags($new_instance['link_energie']) : '';
+        $instance['link_verbraucherunddatenschutz'] = (!empty($new_instance['link_verbraucherunddatenschutz'])) ? strip_tags($new_instance['link_verbraucherunddatenschutz']) : '';
+
         return $instance;
     }
 }
